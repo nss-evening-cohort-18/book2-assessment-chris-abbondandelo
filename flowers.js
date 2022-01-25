8a2911cd72f018f94ac87465597fcda480eb534e
Chris Abbondandelo <76751672+abbondcj@users.noreply.github.com>
2022-01-24 20:11:11 -0600

    successfully completed assessment

---
+++ flowers.js
@@ -0,0 +1,63 @@
+const flowers = [
+    {
+        id: 1,
+        color: "White",
+        species: "Rose",
+        price: 0.90
+    }, 
+    {
+        id: 2,
+        color: "Red",
+        species: "Tulip",
+        price: 1.10
+    },
+]
+
+const addFlower = (flowerObject) => {
+    /*
+        This function should add an `id` property
+        to the object that was sent to the function.
+        The value of the `id` property should be 1
+        greater than the current maximum `id` in the
+        array. Once the `id` property has been added to the,
+        object, the object should then be put into the
+        `flowers` array.
+    */
+
+    let lastFlowerIndex = flowers.length - 1;
+    let lastFlower = flowers[lastFlowerIndex];
+    let lastId = lastFlower.id;
+    let maxId = lastId + 1;
+    flowerObject.id = maxId;
+    flowers.push(flowerObject);
+};
+
+
+const findExpensiveFlowers = () => {
+    const expensiveFlowers = []  // Do not change this code
+
+
+    /*
+        Write a for..of loop that iterate the array
+        of flowers, and if the price of a flower is
+        greater than or equal to 1.00, it should be
+        added to the `expensiveFlowers` array.
+    */
+    for (const flower of flowers) {
+        if (flower.price >= 1.00) {
+            expensiveFlowers.push(flower);
+        };
+    };
+
+
+
+    return expensiveFlowers  // Do not change this code
+}
+
+
+
+// Do not touch this code
+module.exports = {
+    findExpensiveFlowers, addFlower
+}
+
